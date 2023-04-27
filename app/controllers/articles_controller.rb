59dc42db528f22b9971a9408eec0ef75e1f6341e
Andres Munoz <andremude@hotmail.com>
2023-04-27 17:13:31 -0500

    add articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  def index
+    @last_articles = Article.order(created_at: :desc).limit(6)
+    @free_articles = Article.free
+  end
+
+  def show
+    @last_articles = Article.order(created_at: :desc).limit(3)
+    @article = policy_scope(Article).find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created."
+      redirect_to @article
+    else
+      flash[:error] = "Error creating article."
+      render :new
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body, :price, :private)
+    end
+  end
+end
