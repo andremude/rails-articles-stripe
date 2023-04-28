e2ff038d7eae4cabff5af5d63d7fb5b2502282ed
Andres Munoz <andremude@hotmail.com>
2023-04-28 15:12:20 -0500

    add checkout controller

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,20 @@
+class CheckoutController < ApplicationController
+
+  def create
+    order = Order.find(params[:order_id])
+    prices = order.articles.map do |article|
+      { price: article.stripe_pricing_id, quantity: 1 }
+      #Add desciption: article.title
+    end
+
+    session = Stripe::Checkout::Session.create({
+      cancel_url: order_url(order),
+      success_url: root_url,
+      mode: 'payment',
+      payment_method_types: ['card'],
+      line_items: prices
+    })
+
+    redirect_to session.url
+  end
+end
