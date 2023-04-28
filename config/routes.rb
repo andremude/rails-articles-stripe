ae168a2a997e32b50e9417cb62cd9e19bd0a4e60
Andres Munoz <andremude@hotmail.com>
2023-04-28 15:06:09 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles, except: [:destroy]
+  resources :order_items, only: [:create]
+  resources :orders do
+    get '/current', to: 'orders#current', on: :collection
+    resources :checkout, only: [:create]
+    get '/checkout', to: 'orders#checkout'
+  end
+
+  post '/webhook', to:'webhook#receive'
 end
