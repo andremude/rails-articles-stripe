31e64def09acf786a5dd723499061f4343d01cf2
Andres Munoz <andremude@hotmail.com>
2023-04-28 15:13:56 -0500

    add orders_controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def current
+    @last_articles = Article.order(created_at: :desc).limit(3)
+    @order = Order.where(user_id: current_user.id, status: 'created').order(updated_at: :desc).first
+    @order || Order.create(user_id: current_user.id)
+    render :show
+  end
+
+  def show
+    @last_articles = Article.order(created_at: :desc).limit(3)
+    @order = Order.find(params[:id])
+  end
+
+  def checkout
+    order = Order.find(params[:order_id])
+    @intent = Stripe::PaymentIntent.create({
+        amount: order.total,
+        currency: 'usd'
+      })
+    order.update(stripe_payment_id: @intent.id)
+  end
+end
