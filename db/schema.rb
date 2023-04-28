468f003ee92506d79a029ea68674448d18de669b
Andres Munoz <andremude@hotmail.com>
2023-04-28 16:24:58 -0500

    add categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_140506) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_211607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_140506) do
     t.integer "price"
     t.string "stripe_pricing_id"
     t.string "stripe_product_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "stripe_pricing_id"
+    t.string "stripe_product_id"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
